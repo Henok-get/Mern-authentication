735276ed5c55013e6f8b2046b7f6b4c4cbbdfb5d
Henok-get <hgetachew349@gmail.com>
2024-10-03 11:23:23 +0300

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 const app=express()
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("connected to db")
+}).catch((err)=>{
+    console.log(err)
+})
+
+
 
 app.listen(3000,()=>{
    
