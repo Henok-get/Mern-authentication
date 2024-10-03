20c798dd8e1c54a9125b1a19d5f74188a742a6fd
Henok-get <hgetachew349@gmail.com>
2024-10-03 10:41:55 +0300

    create header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 function App() {
  
   return (
     <>
   <BrowserRouter>
+  <Navbar/>
   <Routes>
 
-<Route path="/"  element={<Navbar/>}/>
+<Route path="/"  element={<Home/>}/>
+<Route path="/about"  element={<About/>}/>
+<Route path="/profile"  element={<Profile/>}/>
+<Route path="/signin"  element={<Signin/>}/>
+<Route path="/signup"  element={<Signup/>}/>
 
 
   </Routes>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-<h1 className='text-justify'>hello world</h1>
+<div className='bg-slate-200'>
+    <div className='flex justify-between items-center max-w-6xl max-auto p-3'>
+    <Link to='/'><h1 className='font-bold'>Auth app</h1></Link>
+    <ul className='flex gap-2'>
+        <Link to='/'>
+        <li>home</li>
+        </Link>
+        <Link to='/about'><li>About</li></Link>
+        <Link to='/signin'><li>Sign In</li></Link>
+     
+    </ul>
+    </div>
+    </div>
   )
 }
 
